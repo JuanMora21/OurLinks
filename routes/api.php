4b032d1dc1e1b719af335ab1cf73a562930422e9
JuanMora21 <74797654+JuanMora21@users.noreply.github.com>
2021-02-25 16:19:34 -0500

    agregamos las rutas de api REST

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\api\LinkController;
+use App\Http\Controllers\api\SocialNetworkController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('Links', LinkController::class);
+Route::apiResource('SocialNetworks', SocialNetworkController::class);
